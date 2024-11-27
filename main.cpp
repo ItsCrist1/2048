28936acfd688d8bcd760c63cbbd3a0415050223a
ItsCrist1 <ItsCristiii@gmail.com>
2024-11-27 12:04:00 +0200

    Finished the settings system with saving

--- main.cpp
+++ main.cpp
@@ -109,7 +109,7 @@ void launchLoadMenu() {
             case 'r': r = 1; break;
             
             case 'y':
-            std::wcout << ANSI_CLEAR << L"What do you want to rename " << stw(gs.Title) << "? ";
+            std::wcout << ANSI_CLEAR << L"What do you want to rename " << stw(gs.Title) << L" to? ";
             std::cin >> s;
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
             p = fs::path(SaveDirectory) / fs::path(s + SaveExtension);
@@ -143,24 +143,85 @@ void setDefSettings() {
 
 void loadSettings() {
     std::ifstream is (SettingsPath, std::ios::binary);
+    readu32(is);
     boardSz = readu32(is); 
     is.close();
 }
 
 void saveSettings() {
     std::ofstream os (SettingsPath, std::ios::binary);
+    writeu32(os, ValidationMagicNumber);
     writeu32(os, boardSz);
     os.close();
 }
 
 void LoadSettings() {
-    
+    if(fs::is_regular_file(SettingsPath)) {
+        std::ifstream is (SettingsPath, std::ios::binary);
+        if(readu32(is) == ValidationMagicNumber) {
+            loadSettings();
+            return;
+        }
+    } setDefSettings(); saveSettings();
+}
+
+void drawSettings(const u8& idx) {
+    std::wcout << ANSI_CLEAR
+               << getCol(!idx?SelectedColor:UnselectedColor) << L"1) Change size: " << boardSz << L'\n'
+               << getCol(idx==1?SelectedColor:UnselectedColor) <<  L"2) Back\n"
+               << ANSI_RESET;
+}
+
+bool execSettings(const u8& idx) {
+    switch(idx) {
+        case 0:
+        std::wcout << ANSI_CLEAR << L"Insert new value: ";
+        u32 sz; std::cin >> sz;
+        if(std::cin.fail()) {
+            std::cin.clear();
+            std::wcout << L"\nError, invalid input, must be a value from 1 to " << UINT32_MAX << L"\nPress any key to continue...";
+            getChar();
+        } else {
+            boardSz = sz;
+            saveSettings();
+        }
+        break;
+
+        case 1: return 0;
+    } return 1;
+}
+
+void launchSettingsMenu() {
+    u8 idx = 0;
+    while(1) {
+        drawSettings(idx);
+        const char c = getChar();
+
+        if(std::isdigit(c)) {
+            if(!execSettings(c-'0'-1)) return;
+            continue;
+        }
+
+        switch(c) {
+            case 'w':
+            case 'a': idx += idx ? -1 : 1; 
+            break;
+
+            case 's':
+            case 'd': idx += idx < 1 ? 1 : -1;
+            break;
+            
+            case ' ': if(!execSettings(idx)) return;
+
+            case 'q': return;
+        }
+    }
 }
 
 bool exec(const u8& idx) {
     switch(idx) {
         case 1:
-        Game(Gamesave(getFirstValidName(),4), 1);
+        Game(Gamesave(getFirstValidName(),boardSz), 1);
         break;
 
         case 2:
@@ -169,7 +230,7 @@ bool exec(const u8& idx) {
         break;
 
         case 3:
-        
+        launchSettingsMenu(); 
         break;
 
         case 4: return 0;
@@ -185,11 +246,12 @@ i32 main(i32 argc, char *argv[]) {
     }
 
     if(!fs::is_directory(SaveDirectory)) fs::create_directory(SaveDirectory);
+    LoadSettings();
 
 	#ifdef _WIN32
 	SetConsoleOutputCP(CP_UTF8);
 	#else
-	std::locale::global(std::locale(""));
+	std::locale::global (std::locale(""));
     initTerminalStates();
 	#endif
 
